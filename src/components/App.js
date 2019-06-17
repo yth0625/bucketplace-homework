2cacf11dc7ed072d0c1cc7a4882c5b302352c0ab
Taehyeon Yun <yth0625@gmail.com>
2019-06-17 09:59:00 +0900

    App.js 추가

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ImageList from '../containers/imageListContainer.js';
+
+const App = () => {
+    return (
+        <div className ='App'>
+            <ImageList />
+        </div>
+    );
+}; 
+
+
+export default App;

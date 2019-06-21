e3e61bb879f46deca08e5e2513b60125bcecc4ca
Taehyeon Yun <yth0625@gmail.com>
2019-06-21 16:32:48 +0900

    스크랩 할 떄 Toast ui 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import ImageList from '../containers/imageListContainer.js';
 import './App.css';
 
@@ -7,9 +10,11 @@ const App = () => {
     if (!localStorage.imageList) {
         localStorage.imageList = JSON.stringify([]);
     }
+
     return (
         <div className ='App'>
             <ImageList />
+            <ToastContainer position={toast.POSITION.BOTTOM_CENTER} autoClose={2000} hideProgressBar={true} />
         </div>
     );
 }; 
--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,25 @@ import PropTypes from 'prop-types';
 import onScrap from '../style/blue.svg';
 import offScrap from '../style/on-img.svg';
 
+import { toast } from 'react-toastify';
+
+
 const Image = ({profileImageUrl, nickName, id, imageUrl, scrap, scrapImage}) => {
+    const scrapButtonClick = (id) => {
+        if (scrap)
+            toast('스크랩 취소되었습니다.');
+        else {
+            const ToastElement  = () => (
+                <div className='Toast'>
+                    <div className='ToastImage' style = {{backgroundImage: `url(${imageUrl})`}}/>
+                    <span className='ToastText'>스크랩 하였습니다</span>
+                </div>
+            );
+            toast(<ToastElement/>)
+        }
+        scrapImage(id);
+    }
+
     return (
         <div className = 'ImageComponent'>
             <div className = "ImageHeader">
@@ -13,15 +31,12 @@ const Image = ({profileImageUrl, nickName, id, imageUrl, scrap, scrapImage}) => 
             </div>
             <div className = 'PictureImage' style={{backgroundImage: `url(${imageUrl})`}}>
                 <img 
-                        onClick = {() => scrapImage(id)}
+                        onClick = {() => scrapButtonClick(id)}
                         className ='ScrapButton'
                         src={`${scrap  ? onScrap : offScrap}`}
                         alt= 'scrap'
                 />
             </div>
-            
-            
-            
         </div>
     );
 };
